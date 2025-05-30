ddb384b94929b5c61cd5d0564a2af5a2395227df
Racheal_A <mrswalter404@yahoo.com>
2025-05-30 06:49:09 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "state-store-35" # same as above
+    bucket         = "state-store-36" # same as above
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "State-lock-35"
+    dynamodb_table = "State-lock-36"
     encrypt        = true
   }
 }
